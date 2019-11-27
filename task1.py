4fc82e858f309006a07e404e918803c9a8e2e9b5
Baygazy <bgzmkbull@gmail.com>
2019-11-28 00:19:08 +0600

    chapter4_task1

---
+++ task1.py
@@ -0,0 +1,20 @@
+class Student():
+    name = "Vasya"
+    def __init__(self, name, last_name, department, year_of_entrance):
+        self.name = name
+        self.last_name = last_name
+        self.department = department
+        self.year_of_entrance = year_of_entrance
+
+    def get_student_info(self):
+        name = self.name
+        last_name = self.last_name
+        department = self.department
+        year_of_entrance = self.year_of_entrance
+        message = f"{name.title()} {last_name.title()} поступил в {year_of_entrance} г. на факультет {department}."
+        return message
+
+go = Student("Vasya", "Ivanov", "programirovanie", "2019")
+print(go.get_student_info())
+
+
